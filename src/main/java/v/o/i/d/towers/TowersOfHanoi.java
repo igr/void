1457dd4f1f4630e6bcb61e0219a560ae16e4e0e1
Igor Spasić <igor.spasic@gmail.com>
2017-10-17 17:17:38 +0200

    Added Towers of Hanoi

---
+++ src/main/java/v/o/i/d/towers/TowersOfHanoi.java
@@ -0,0 +1,45 @@
+package v.o.i.d.towers;
+
+/**
+ * The Tower of Hanoi (sometimes referred to as the Tower of Brahma or the End of the World Puzzle)
+ * was invented by the French mathematician, Edouard Lucas, in 1883. He was inspired by a legend that
+ * tells of a Hindu temple where the pyramid puzzle might have been used for the mental discipline
+ * of young priests. Legend says that at the beginning of time the priests in the temple were given a
+ * stack of 64 gold disks, each one a little smaller than the one beneath it. Their assignment was to
+ * transfer the 64 disks from one of the three poles to another, with one important provisoøa large disk
+ * could never be placed on top of a smaller one. The priests worked very efficiently, day and night.
+ * When they finished their work, the myth said, the temple would crumble into dust and the world would vanish.
+ *
+ * The number of separate transfers of single disks the priests must make to transfer the tower is 2 to the 64th minus 1,
+ * or 18,446,744,073,709,551,615 moves! If the priests worked day and night, making one move every second it would take
+ * slightly more than 580 billion years to accomplish the job!
+ *
+ */
+public class TowersOfHanoi {
+
+	static int moves;
+
+	/**
+	 * Recursive solution.
+	 * O(2^N) 
+	 */
+	static void hanoi(int height, int fromPole, int toPole, int withPole) {
+		if (height >= 1) {
+			hanoi(height - 1, fromPole, withPole, toPole);
+			moveDisk(fromPole, toPole);
+			hanoi(height - 1, withPole, toPole, fromPole);
+		}
+	}
+
+	static void moveDisk(int fromPole, int toPole) {
+		moves++;
+		System.out.print(fromPole);
+		System.out.print(toPole);
+		System.out.print(((moves % 20) == 0) ? '\n' : ' ');
+	}
+
+
+	public static void main(String[] args) {
+		hanoi(5, 1, 2, 3);
+	}
+}
---
+++ src/main/java/v/o/i/d/towers/TowersOfHanoiIterative.java
@@ -0,0 +1,107 @@
+package v.o.i.d.towers;
+
+
+public class TowersOfHanoiIterative {
+
+	int[] disks;    // represents current position of the disks.
+					// disk[i] == p means that disk i is on pole p(1,2,3)
+
+	int moves;
+	int n;
+
+	public TowersOfHanoiIterative(int n) {
+		this.n = n;
+		this.moves = 1;
+		disks = new int[n];
+		for (int i = 0; i < disks.length; i++) {
+			disks[i] = 1;
+		}
+	}
+
+	public void run() {
+		while(true) {
+			if (moves == 1) {
+				performTheFirstMove();
+			} else {
+				moveSmallestDiskClockwise();
+			}
+			if (isTheEnd()) {
+				break;
+			}
+			performOnlyAvailableMoveNotInvolvingSmallestDisk();
+			if (isTheEnd()) {
+				break;
+			}
+		}
+	}
+
+	private void performTheFirstMove() {
+		if (n % 2 == 0) {
+			disks[n - 1] = 2;
+			out(1, 2);
+		} else {
+			disks[n - 1] = 3;
+			out(1, 3);
+		}
+	}
+
+	private void performOnlyAvailableMoveNotInvolvingSmallestDisk() {
+		int smallestPole = disks[n - 1];
+		int poleA, poleB;
+		switch (smallestPole) {
+			case 1: poleA = 2; poleB = 3; break;
+			case 2: poleA = 3; poleB = 1; break;
+			case 3: poleA = 1; poleB = 2; break;
+			default:
+				throw new IllegalStateException("The smallest disk is not on existing pole.");
+		}
+		int diskA = findFirstDiskOnPole(poleA);
+		int diskB = findFirstDiskOnPole(poleB);
+		if (diskA < diskB) {
+			disks[diskB] = poleA;
+			out(poleB, poleA);
+		} else if (diskA > diskB) {
+			disks[diskA] = poleB;
+			out(poleA, poleB);
+		} else {
+			throw new IllegalStateException("Two disks of the same size on two poles: " + poleA + " and " + poleB);
+		}
+	}
+
+	private int findFirstDiskOnPole(int pole) {
+		for (int i = n - 1; i >= 0; i--) {
+			if (disks[i] == pole) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private boolean isTheEnd() {
+		moves++;
+		for (int disk : disks) {
+			if (disk != 3) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void moveSmallestDiskClockwise() {
+		int before = disks[n - 1];
+		disks[n - 1]++;
+		if (disks[n - 1] > 3) {
+			disks[n - 1] =  1;
+		}
+		out(before, disks[n - 1]);
+	}
+
+	private void out(int from, int to) {
+		System.out.println(moves + "\t" + from + "->" + to);
+	}
+
+	public static void main(String[] args) {
+		TowersOfHanoiIterative tohi = new TowersOfHanoiIterative(4);
+		tohi.run();
+	}
+}
