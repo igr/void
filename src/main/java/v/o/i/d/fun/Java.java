cf9bbf73f8204d5cef254c626759956379955d4e
Igor Spasić <igor.spasic@gmail.com>
2017-12-23 13:33:45 +0100

    Another art

---
+++ src/main/java/v/o/i/d/fun/Java.java
@@ -0,0 +1,43 @@
+package v.o.i.d.fun;
+
+/* coded by ++najgor */
+
+public class Java {
+
+String $J =
+		"BWDBM$W@DHMHW@MN8RWOMHMADOWOWWABOAM0WD" +
+		"BMODMQM0WMOMMBARBMO0MQBAW@OBWWMAW60DMO" +
+		"WBABODW#                       M0MAD8A" +
+		"8WMMND#W                       @MRWND$" +
+		"MQBAWOD6WBMNWBD$M0BAM$O8       W6A8WAD" +
+		"O0#WRMWO8W6MAWHD#A8MAMND       WWHM0WN" +
+		"D8WOWWABWWWWWWMB0$A8DWAB       OWWBMOW" +
+		"HD#MNBAM$8RWAWBMBDBMQW6A       BDQW$WD" +
+		"WODOM#MWABM#WWWABMODMQMH       M$D#WWM" +
+		"NW@0DMOWBABDQWB0#WWD6WOW       #WOOBW@" +
+		"A8MBD6MRWHWMOQW6WOWB0DW        DBAW8ON" +
+		"WAA8ONDOWWWBO0D#WBWOWW0       $0#8HD@O" +
+		"NM0M0DOOAMDMMABOWWBA8W        @O0WWWRW" +
+		"8OHWHM0       BMMQM0         MDMA8RMNM" +
+		"0WND8MO        WNM$O        QWW0#MNDOA" +
+		"8M8WOOHB        MM$        W@DQMHW6MA8" +
+		"RWNMDBMMQM                0MDMA8RM0M0M" +
+		"ND$MHWHMA8RW@           MOBMOWM6WBARBW" +
+		"8NDOW@DMWBWRMWOAMOMRW60O0#8HOWM6WBARBW" +
+		"BMODMQM0WMOMMBARBMO0MQBAW@OBWWMAW60DMO";
+
+
+
+	String $$$="JavaSvet";String $$="WMODBA806QRN#$@H";
+	byte[] $;int nu1l, whi1e;public void Svet(){if($ ==
+	null){$=$J.getBytes();} if(nu1l>=711){return;}while
+	($[nu1l]==0x20)nu1l++;int __=$$. indexOf($[nu1l++])
+	*020;while($[nu1l]== 040) nu1l++;__+= $$.indexOf($[
+	nu1l++] );__^=$$$.charAt(whi1e);if($$$.length()==++
+	whi1e){whi1e=0;}System.out.print((char)__);Svet();}
+
+
+	public static void main(String[] args) {
+		new Java().Svet();
+	}
+}
