4a80b3379a517e4159a9a6a06bde61d161bb60ae
Igor Spasić <igor.spasic@gmail.com>
2017-12-25 12:13:46 +0100

    Schedule at fixed rate

--- src/main/java/v/o/i/d/concurrent/SingleThreadExecutor.java
+++ src/main/java/v/o/i/d/concurrent/SingleThreadExecutor.java
@@ -1,17 +1,21 @@
 package v.o.i.d.concurrent;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class SingleThreadExecutor {
 
 	static Runnable runnable = () -> System.out.println(Thread.currentThread().getName());
 
 	public static void main(String[] args) {
-		Executor ex1 = Executors.newSingleThreadScheduledExecutor();
-		Executor ex2 = Executors.newSingleThreadScheduledExecutor();
+		ScheduledExecutorService ex1 = Executors.newSingleThreadScheduledExecutor();
+		ScheduledExecutorService ex2 = Executors.newSingleThreadScheduledExecutor();
 
 		ex1.execute(runnable);
 		ex2.execute(runnable);
+
+		ex1.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
+		ex2.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
 	}
 }
