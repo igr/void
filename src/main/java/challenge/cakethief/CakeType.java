9df2e5e021acf6aea75db1253152511784ab5b53
Igor Spasić <igor.spasic@gmail.com>
2020-10-01 14:56:59 +0200

    thief

---
+++ src/main/java/challenge/cakethief/CakeType.java
@@ -0,0 +1,11 @@
+package challenge.cakethief;
+
+public class CakeType {
+	final int weight;
+	final int value;
+
+	public CakeType(int weight, int value) {
+		this.weight = weight;
+		this.value  = value;
+	}
+}
