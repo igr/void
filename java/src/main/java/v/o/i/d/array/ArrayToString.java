27f7d08cd8d068c8bef690cc465a0d1fd882bae2
Igor Spasić <igor.spasic@gmail.com>
2023-09-07 11:16:02 +0200

    Array toString

---
+++ java/src/main/java/v/o/i/d/array/ArrayToString.java
@@ -0,0 +1,12 @@
+package v.o.i.d.array;
+
+import java.util.Arrays;
+
+public class ArrayToString {
+
+	public static void main(String[] args) {
+		int[] groupId = new int[0];
+		System.out.println("Group not found: " + groupId);
+		System.out.println("Group not found: " + Arrays.toString(groupId));
+	}
+}
