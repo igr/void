34e0e536f7ee56c3f686e6cb4295e218bb980547
Igor Spasić <igor.spasic@gmail.com>
2024-10-23 10:09:26 +0200

    iom

---
+++ java/src/main/java/v/o/i/d/iomonad/GuessingGame.java
@@ -0,0 +1,80 @@
+package v.o.i.d.iomonad;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+
+	private static final PrintStream out = System.out;
+	private static final InputStream in = System.in;
+
+	public static void main(String[] args) {
+		IoMonad<Void> game = IoMonad
+				.of(() -> out.println("Welcome to the Guessing Game!"))
+				.bind(ignored -> generateRandomNumber())
+				.map(number -> number + 1)  // [0, 100) -> [1, 100]
+				.bind(number -> IoMonad.of(() -> {
+					out.println("I'm thinking of a number between 1 and 100.");
+					return number;
+				}))
+				.bind(GuessingGame::guessLoop);
+
+		try {
+			game.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Generates a number [0, 100)
+	private static IoMonad<Integer> generateRandomNumber() {
+		return IoMonad.of(() -> new Random().nextInt(100));
+	}
+
+	private static IoMonad<Void> guessLoop(int number) {
+		return IoMonad
+				.of(() -> out.print("Take a guess: "))
+				.bind(ignored -> readUserGuess())
+				.bind(guess -> checkUserInput(guess, number));
+	}
+
+	private static IoMonad<String> readUserGuess() {
+		return IoMonad.of(() -> new Scanner(in).nextLine());
+	}
+
+	private static IoMonad<Void> checkUserInput(String guess, int number) {
+		return IoMonad
+				.of(() -> {
+					try {
+						return Integer.parseInt(guess);
+					} catch (NumberFormatException e) {
+						return null;    // well...
+					}
+				})
+				.bind(userGuess -> {
+					if (userGuess == null) {
+						return IoMonad
+								.of(() -> out.println("Please enter a valid number."))
+								.bind(ignored -> guessLoop(number));
+					} else {
+						return checkGuess(userGuess, number);
+					}
+				});
+	}
+
+	private static IoMonad<Void> checkGuess(int guess, int number) {
+		if (guess < number) {
+			return IoMonad
+					.of(() -> out.println("Too low! Try again."))
+					.bind(ignored -> guessLoop(number));
+		} else if (guess > number) {
+			return IoMonad
+					.of(() -> out.println("Too high! Try again."))
+					.bind(ignored -> guessLoop(number));
+		} else {
+			return IoMonad.of(() -> out.println("Congratulations! You guessed the right number!"));
+		}
+	}
+}
---
+++ java/src/main/java/v/o/i/d/iomonad/IoMonad.java
@@ -0,0 +1,54 @@
+package v.o.i.d.iomonad;
+
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
+public sealed interface IoMonad<T> {
+
+	<R> IoMonad<R> map(Function<T, R> mapper);
+
+	// Haskell's `>>=` operator (bind)
+	<R> IoMonad<R> bind(Function<T, IoMonad<R>> mapper);
+
+	T run() throws Exception;
+
+	// Haskell's `>>` operator (then)
+	static <T> IoMonad<T> of(Callable<T> computation) {
+		return new DefaultIoMonad<>(computation);
+	}
+
+	// just a syntax sugar
+	static <T> IoMonad<T> of(Runnable computation) {
+		return new DefaultIoMonad<>(() -> {
+			computation.run();
+			return null;
+		});
+	}
+
+	default <R> IoMonad<R> then(IoMonad<R> next) {
+		return this.bind(ignored -> next);
+	}
+
+	final class DefaultIoMonad<T> implements IoMonad<T> {
+		private final Callable<T> computation;
+
+		DefaultIoMonad(Callable<T> computation) {
+			this.computation = computation;
+		}
+
+		@Override
+		public <R> IoMonad<R> map(Function<T, R> mapper) {
+			return new DefaultIoMonad<>(() -> mapper.apply(computation.call()));
+		}
+
+		@Override
+		public <R> IoMonad<R> bind(Function<T, IoMonad<R>> mapper) {
+			return new DefaultIoMonad<>(() -> mapper.apply(computation.call()).run());
+		}
+
+		@Override
+		public T run() throws Exception {
+			return computation.call();
+		}
+	}
+}
