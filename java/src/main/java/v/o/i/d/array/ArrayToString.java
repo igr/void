2f545c2b892df3ad5b1099076e49541e7ef962a1
Igor Spasić <igor.spasic@gmail.com>
2025-05-11 19:01:35 +0200

    Better example

--- java/src/main/java/v/o/i/d/array/ArrayToString.java
+++ java/src/main/java/v/o/i/d/array/ArrayToString.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class ArrayToString {
 
 	public static void main(String[] args) {
-		int[] groupId = new int[0];
+		int[] groupId = new int[1];
+		groupId[0] = 1;
 		System.out.println("Group not found: " + groupId);
 		System.out.println("Group not found: " + Arrays.toString(groupId));
 	}
